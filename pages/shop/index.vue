926cbc2b8221bc7faa4e01f9d830cb8976958134
Kasper <kasperkh.kh@gmail.com>
2019-09-03 15:20:18 +0200

    Fixed broken image src links

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -21,35 +21,35 @@ export default {
         items: [
           {
             url: 'https://open.spotify.com/track/4m4SB61NXgwmfZm37incal?si=HztNWM0aSiyTICviSj4lIg',
-            src: 'thumbnails/cover-art/serion-x-dabl-catch-you-in-my-arms.jpg',
+            src: require('~/assets/thumbnails/cover-art/serion-x-dabl-catch-you-in-my-arms.jpg'),
           },
           {
             url: 'https://open.spotify.com/track/6O2DrzgrHG5GrXX4pihPlW?si=r9oDt-EVSEukCF2d2HYSfQ',
-            src: 'thumbnails/cover-art/barren-gates-x-serion-deep-within.jpg',
+            src: require('~/assets/thumbnails/cover-art/barren-gates-x-serion-deep-within.jpg'),
           },
           {
             url: 'https://open.spotify.com/track/6mZIHwepGYSSrdeJCSNXZi?si=FrCvhi0cRSuFumQlcCO5tw',
-            src: 'thumbnails/cover-art/serion-x-lotis-in-the-end.jpg',
+            src: require('~/assets/thumbnails/cover-art/serion-x-lotis-in-the-end.jpg'),
           },
           {
             url: 'https://open.spotify.com/track/5c9O1rlHCNqyJGFVeMN6V2?si=pSHPpEZ2TWa-I5DcEirtyA',
-            src: 'thumbnails/cover-art/serion-broken.jpg',
+            src: require('~/assets/thumbnails/cover-art/serion-broken.jpg'),
           },
           {
             url: 'https://open.spotify.com/track/3ju2YEKOQikMgetgj53xUJ?si=YXqZDFQgQDaLXNRr8a8QrQ',
-            src: 'thumbnails/cover-art/voldex-colorblind-feat-cluda.jpg',
+            src: require('~/assets/thumbnails/cover-art/voldex-colorblind-feat-cluda.jpg'),
           },
           {
             url: '',
-            src: 'thumbnails/cover-art/backblur-1.jpg',
+            src: require('~/assets/thumbnails/cover-art/backblur-1.jpg'),
           },
           {
             url: '',
-            src: 'thumbnails/cover-art/snow-forest.jpg',
+            src: require('~/assets/thumbnails/cover-art/snow-forest.jpg'),
           },
           {
             url: '',
-            src: 'thumbnails/cover-art/starweb-2.jpg',
+            src: require('~/assets/thumbnails/cover-art/starweb-2.jpg'),
           },
         ],
       },
