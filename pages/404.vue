818eda353f8656998683c891b5674a20ec4af369
Kasper <kasperkh.kh@gmail.com>
2019-09-16 04:27:46 +0200

    Added 404 page in last commit

--- pages/404.vue
+++ pages/404.vue
@@ -19,32 +19,3 @@ export default {
   },
 }
 </script>
-
-<style lang='sass' scoped>
-.page-container
-  white-space: pre-wrap
-  a
-    display: inline-block
-    position: relative
-    padding: 3px
-    .text
-      transition: all 0.2s cubic-bezier(0.4, 0, 0.2, 1)
-    &::after
-      content: ''
-      position: absolute
-      width: 100%
-      height: 2px
-      background-color: #d3d4de
-      bottom: 0px
-      left: 0
-      opacity: 0
-      transform: scaleX(0.7)
-      transition: all 0.2s cubic-bezier(0.4, 0, 0.2, 1)
-    &:hover
-      $moveVertically: -2px
-      .text
-        transform: translateY($moveVertically)
-      &::after
-        transform: translateY($moveVertically) scaleX(1)
-        opacity: 1
-</style>
