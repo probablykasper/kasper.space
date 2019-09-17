2faa16d8db6c436e6a05058956fdfa36dd944eb0
Kasper <kasperkh.kh@gmail.com>
2019-09-17 20:30:47 +0200

    Changed text on contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,7 +3,7 @@
     h1(data-aos='zoom-out' data-aos-duration='750' data-aos-delay='0')
       | Contact
     p(data-aos='zoom-out' data-aos-duration='750' data-aos-delay='0')
-      | You can reach me at #[a(href='mailto:kasperkh.kh@gmail.com') #[.text kasperkh.kh@gmail.com]] or on #[a(href='https://facebook.com/kasper.space') #[.text Facebook]].
+      | You can reach me at #[a(href='mailto:kasperkh.kh@gmail.com') #[.text kasperkh.kh@gmail.com]], <b>Kasper#6452</b> on Discord or on #[a(href='https://facebook.com/kasper.space') #[.text Facebook]].
 </template>
 
 <script>
@@ -26,31 +26,34 @@ export default {
 <style lang='sass' scoped>
 .page-container
   white-space: pre-wrap
-  a
+p
+  max-width: 340px
+a, b
+  color: #ebebef
+  font-weight: bold
+a
+  display: inline-block
+  position: relative
+  padding: 3px
+  .text
+    transition: all 0.2s cubic-bezier(0.4, 0, 0.2, 1)
     display: inline-block
-    position: relative
-    padding: 3px
-    color: #ebebef
-    font-weight: bold
+  &::after
+    content: ''
+    position: absolute
+    width: 100%
+    height: 2px
+    background-color: #d3d4de
+    bottom: 0px
+    left: 0
+    opacity: 0
+    transform: scaleX(0.7)
+    transition: all 0.2s cubic-bezier(0.4, 0, 0.2, 1)
+  &:hover
+    $moveVertically: -2px
     .text
-      transition: all 0.2s cubic-bezier(0.4, 0, 0.2, 1)
-      display: inline-block
+      transform: translateY($moveVertically)
     &::after
-      content: ''
-      position: absolute
-      width: 100%
-      height: 2px
-      background-color: #d3d4de
-      bottom: 0px
-      left: 0
-      opacity: 0
-      transform: scaleX(0.7)
-      transition: all 0.2s cubic-bezier(0.4, 0, 0.2, 1)
-    &:hover
-      $moveVertically: -2px
-      .text
-        transform: translateY($moveVertically)
-      &::after
-        transform: translateY($moveVertically) scaleX(1)
-        opacity: 1
+      transform: translateY($moveVertically) scaleX(1)
+      opacity: 1
 </style>
