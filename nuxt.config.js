2b8b524f6fe0242799a668324e2fe22179bcbf41
Kasper <kasperkh.kh@gmail.com>
2023-01-19 11:11:35 +0100

    Add meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'description', name: 'description', content: "Norwegian software developer, VFX artist, record label manager and mammal" },
       // favicon stuff:
       { name: 'msapplication-TileColor', content: '#000000' },
       { name: 'theme-color', content: '#000000' },
