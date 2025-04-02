fd6fcc710c19b026ad7bb738ae4ba5fc37b103c9
Kasper <kasperkh.kh@gmail.com>
2025-04-02 06:10:59 +0200

    Reorder social links

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,10 @@
     .icons
       a(href='https://bsky.app/profile/kasper.space' aria-label="BlueSky" data-aos='zoom-out' data-aos-duration='750' data-aos-delay='100')
         BlueSkyIcon
-      a(href='https://twitter.com/probablykasper' aria-label="Twitter" data-aos='zoom-out' data-aos-duration='750' data-aos-delay='100')
-        TwitterIcon
       a(href='https://github.com/probablykasper' aria-label="GitHub" data-aos='zoom-out' data-aos-duration='750' data-aos-delay='150')
         GitHubIcon
+      a(href='https://twitter.com/probablykasper' aria-label="Twitter" data-aos='zoom-out' data-aos-duration='750' data-aos-delay='100')
+        TwitterIcon
       a(href='https://instagram.com/probablykasper' aria-label="Instagram" data-aos='zoom-out' data-aos-duration='750' data-aos-delay='200')
         InstagramIcon
       a(href='https://www.linkedin.com/in/kasper-henningsen' aria-label="LinkedIn" data-aos='zoom-out' data-aos-duration='750' data-aos-delay='250')
@@ -20,8 +20,8 @@ import CenterView from '~/components/CenterView.vue'
 import { EventBus } from '~/plugins/event-bus.js'
 
 import BlueSkyIcon from '~/assets/icons/bluesky.svg'
-import TwitterIcon from '~/assets/icons/twitter.svg'
 import GitHubIcon from '~/assets/icons/github.svg'
+import TwitterIcon from '~/assets/icons/twitter.svg'
 import InstagramIcon from '~/assets/icons/instagram.svg'
 import LinkedInIcon from '~/assets/icons/linkedin.svg'
 
@@ -29,8 +29,8 @@ export default {
   components: {
     CenterView,
     BlueSkyIcon,
-    TwitterIcon,
     GitHubIcon,
+    TwitterIcon,
     InstagramIcon,
     LinkedInIcon,
   },
